ca5b0df6b47eab9210a99a1903a9165018020eeb
shefali259 <shalu1996@gmail.com>
2019-09-05 16:45:55 +0530

    Insert function and siaply function have been added, all other functions have been updated

--- Trade Surveillance - Front Running/src/com/dao/TradeListDAO.java
+++ Trade Surveillance - Front Running/src/com/dao/TradeListDAO.java
@@ -1,5 +1,8 @@
 package com.dao;
 
+import java.sql.Connection;
+import java.util.List;
+
 import com.pojo.TradeList;
 
 
@@ -8,6 +11,9 @@ public interface TradeListDAO {
 	public int addTradeList(TradeList tradeList);
 	public TradeList findTradeListByID(int tradeListID);
 	public TradeList fetchBySr(int Sr, Connection conn);
-	public void genTrades(int genTradeQuantity)
+	public void genTrades(int genTradeQuantity);
+	int insertBetween(TradeList tradelist);
+	List<TradeList> display();
+	
 
 }
