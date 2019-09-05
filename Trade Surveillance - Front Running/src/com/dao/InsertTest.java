49fdef3b7b347d5190d625145487320cf71ad9d0
shefali259 <shalu1996@gmail.com>
2019-09-05 16:51:22 +0530

    Updates made to insert and showAll

---
+++ Trade Surveillance - Front Running/src/com/dao/InsertTest.java
@@ -0,0 +1,34 @@
+package com.dao;
+
+import java.sql.Timestamp;
+import java.util.List;
+
+import com.pojo.TradeList;
+import com.pojo.Trader;
+
+public class InsertTest {
+
+	public static void main(String[] args) {
+		
+		TradeListDAO dao = new TradeListDAOImpl();
+		
+		TradeList t1=dao.findTradeListByID(6);
+		Timestamp ts=t1.getTimeStamp();
+		ts.setSeconds(ts.getSeconds()+1);
+		
+		Trader trader=new Trader(11, "adc");
+
+		
+		TradeList trade=new TradeList(7, ts, trader, "sell", "futures", 46, 74, "abc", "fb");
+		
+		int i=dao.insertBetween(trade);
+		System.out.println(i);
+		
+		List<TradeList> tr2=dao.display();
+		tr2.forEach(System.out::println);
+		
+		
+
+	}
+
+}
--- Trade Surveillance - Front Running/src/com/dao/TradeListDAO.java
+++ Trade Surveillance - Front Running/src/com/dao/TradeListDAO.java
@@ -8,7 +8,7 @@ import com.pojo.TradeList;
 
 public interface TradeListDAO {
 	
-	public int addTradeList(TradeList tradeList);
+	//public int addTradeList(TradeList tradeList);
 	public TradeList findTradeListByID(int tradeListID);
 	public TradeList fetchBySr(int Sr, Connection conn);
 	public void genTrades(int genTradeQuantity);
