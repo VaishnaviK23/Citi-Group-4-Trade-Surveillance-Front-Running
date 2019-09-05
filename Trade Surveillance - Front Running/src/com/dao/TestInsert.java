a038b8ebce37cc597e755df2f256ba4e6496f2be
shefali259 <shalu1996@gmail.com>
2019-09-05 18:03:10 +0530

    UI compatible

--- Trade Surveillance - Front Running/src/com/dao/TestInsert.java
+++ Trade Surveillance - Front Running/src/com/dao/TestInsert.java
@@ -3,50 +3,56 @@ package com.dao;
 import java.sql.Timestamp;
 import java.util.Date;
 import java.util.List;
-import java.util.Scanner;
+
+import javax.json.JsonObject;
+import javax.json.JsonObject;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
 
 import com.pojo.TradeList;
 import com.pojo.Trader;
 
-public class TestInsert {
+@Path("/check")
+class Test {
 
-	public static void main(String[] args) {
+	@POST
+	@Produces(MediaType.APPLICATION_JSON)
+	@Consumes(MediaType.APPLICATION_JSON)
+
+	public TradeList incoming(JsonObject incomingData) {
+
 		TradeListDAO dao = new TradeListDAOImpl();
-		
 
-		Scanner scanner = new Scanner(System.in);
-		System.out.println("Enter Trade ID: ");
-		int tradeID = scanner.nextInt();
-		
-		System.out.println("Enter trader id: ");
+		String tradeID_ui = incomingData.getString("tradeID");
+		String traderID_ui = incomingData.getString("traderID");
+		String brokerName_ui = incomingData.getString("brokerName");
+		String quantity_ui = incomingData.getString("quantity");
+		String price_ui = incomingData.getString("price");
+		String tradeType_ui = incomingData.getString("tradeType");
+		String securityType_ui = incomingData.getString("securityType");
+		String company_ui = incomingData.getString("company");
 
-		int traderID = scanner.nextInt();
-		
-		System.out.println("Enter trader name: ");
-		String traderName = scanner.next();
-		
-		Trader trader=new Trader(traderID, traderName);
-		
-		System.out.println("Enter price: ");
-		float price = scanner.nextFloat();
-		
-		System.out.println("Enter quantity: ");
+		int traderID = Integer.parseInt(traderID_ui);
 
-		int quantity = scanner.nextInt();
-		System.out.println("Enter security: ");
+		Trader trader = new Trader();
+		String traderName = trader.getTraderName();
+		Trader trader1 = new Trader(traderID, traderName);
 
-		String security = scanner.next();
-		System.out.println("Enter trade type: ");
-		String tradeType = scanner.next();
+		int tradeID = Integer.parseInt(tradeID_ui);
 
-		
-		
-		System.out.println("Enter broker name: ");
+		String brokerName = brokerName_ui;
+		int quantity = Integer.parseInt(quantity_ui);
+		float price = Float.parseFloat(price_ui);
+		String tradeType = tradeType_ui;
+		String company = company_ui;
+		String securityType = securityType_ui;
 
-		String brokerName = scanner.next();
-		System.out.println("Enter company name: ");
-
-		String company = scanner.next();
 		Date date = new Date();
 
 		long time = date.getTime();
@@ -54,12 +60,63 @@ public class TestInsert {
 		Timestamp ts = new Timestamp(time);
 		System.out.println(ts);
 
-		TradeList tradelist = new TradeList(tradeID, ts, trader, tradeType, security, quantity, price, brokerName, company);
+		TradeList tradelist = new TradeList(tradeID, ts, trader1, tradeType, securityType, quantity, price, brokerName,
+				company);
+
 		int inserted = dao.insertBetween(tradelist);
 		System.out.println(inserted);
-		
-		List<TradeList> list=dao.display();
-		list.forEach(System.out::println);
-	}
 
+		List<TradeList> list = dao.display();
+		// list.forEach(System.out::println);
+		JSONArray list1 = new JSONArray();
+
+		for (int i = 0; i < list.size(); i++) {
+			JSONObject eachData = new JSONObject();
+			eachData.put("tradeID", list.get(i).getTradeID());
+			eachData.put("price", list.get(i).getPrice());
+			list1.add(i, eachData);
+
+		}
+
+		JSONObject root = new JSONObject();
+		root.put("data", list1);
+		System.out.println(list1);
+
+		return tradelist;
+
+	}
 }
+
+//	public class TestInsert {
+//
+//		public void main(String[] args) {
+//
+//			TradeListDAO dao = new TradeListDAOImpl();
+//
+////		
+//			Test o = new Test();
+//
+//			TradeList tradelist = o.incoming(incomingData);
+//			int inserted = dao.insertBetween(tradelist);
+//			System.out.println(inserted);
+//
+//			List<TradeList> list = dao.display();
+//			// list.forEach(System.out::println);
+//			JSONArray list1 = new JSONArray();
+//
+//			for (int i = 0; i < list.size(); i++) {
+//				JSONObject eachData = new JSONObject();
+//				eachData.put("tradeID", list.get(i).getTradeID());
+//				eachData.put("price", list.get(i).getPrice());
+//				list1.add(i, eachData);
+//
+//			}
+//
+//			JSONObject root = new JSONObject();
+//			root.put("data", list1);
+//			System.out.println(list1);
+//
+//		}
+//
+//	}
+//}
