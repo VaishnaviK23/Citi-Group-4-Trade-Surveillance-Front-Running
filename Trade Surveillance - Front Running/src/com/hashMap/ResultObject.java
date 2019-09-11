7c18f2989e1c4968c12f8dca13aed222bdec6da1
VaishnaviK23 <vaishnavik.impressions@gmail.com>
2019-09-11 17:50:15 +0530

    Added ResultObject

---
+++ Trade Surveillance - Front Running/src/com/hashMap/ResultObject.java
@@ -0,0 +1,77 @@
+package com.hashMap;
+
+import java.util.ArrayList;
+
+import com.dao.TradeListDAOImpl;
+import com.pojo.TradeList;
+
+public class ResultObject {
+	
+	private TradeList currentTrade;
+	private TradeInfo suspiciousTrades;
+	private TradeInfo frontRunningTrades;
+	private ArrayList<TradeList> listOfSuspiciousTrades;
+	private ArrayList<TradeList> listOfFrontRunningTrades;
+	private int scenarioNumber;
+	
+	
+	
+	public ArrayList<TradeList> getListOfSuspiciousTrades() {
+		return listOfSuspiciousTrades;
+	}
+
+	public void setListOfSuspiciousTrades(ArrayList<TradeList> listOfSuspiciousTrades) {
+		ArrayList<Integer> tempList = this.suspiciousTrades.getTradeNumberList();
+		for (Integer tradeId : tempList) {
+			this.listOfSuspiciousTrades.add(new TradeListDAOImpl().findTradeListByID(tradeId));
+		}
+	}
+
+	public ArrayList<TradeList> getListOfFrontRunningTrades() {
+		return listOfFrontRunningTrades;
+	}
+
+	public void setListOfFrontRunningTrades(ArrayList<TradeList> listOfFrontRunningTrades) {
+		ArrayList<Integer> tempList = this.frontRunningTrades.getTradeNumberList();
+		for (Integer tradeId : tempList) {
+			this.listOfFrontRunningTrades.add(new TradeListDAOImpl().findTradeListByID(tradeId));
+		}
+	}
+
+	
+	
+	
+	
+	public TradeList getCurrentTrade() {
+		return currentTrade;
+	}
+
+	public void setCurrentTrade(TradeList currentTrade) {
+		this.currentTrade = currentTrade;
+	}
+	
+	public ResultObject(TradeList currentTrade, TradeInfo suspiciousTrades, TradeInfo frontRunningTrades, int scenarioNumber) {
+		this.scenarioNumber = scenarioNumber;
+		this.currentTrade = currentTrade;
+		this.suspiciousTrades = suspiciousTrades;
+		this.frontRunningTrades = frontRunningTrades;
+		this.setListOfSuspiciousTrades(this.listOfSuspiciousTrades);
+		this.setListOfFrontRunningTrades(this.listOfFrontRunningTrades);
+	}
+
+	public int getScenarioNumber() {
+		return scenarioNumber;
+	}
+
+	public void setScenarioNumber(int scenarioNumber) {
+		this.scenarioNumber = scenarioNumber;
+	}
+
+	@Override
+	public String toString() {
+		return "ResultObject [currentTrade=" + currentTrade + ", suspiciousTrades=" + suspiciousTrades
+				+ ", frontRunningTrades=" + frontRunningTrades + ", listOfSuspiciousTrades=" + listOfSuspiciousTrades
+				+ ", listOfFrontRunningTrades=" + listOfFrontRunningTrades + ", scenarioNumber=" + scenarioNumber + "]";
+	}
+
+}
